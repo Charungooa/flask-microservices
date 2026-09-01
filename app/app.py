2dac30b70fe550ec076ea0471dc19aa4c9fc5104
Amit <v-2amitc@hotmail.com>
2026-09-01 10:43:38 +0530

    added project files

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify(message="hello from flask-microservice", status="ok")
+
+@app.route("/healthz")
+def healthz():
+    return jsonify(status="healthy"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
